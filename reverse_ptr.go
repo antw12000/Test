714413ec7f712c8c2aa1b11fe4bd578ae1486713
Antoine Sewell <asewell@asewell-mbp15.hughestelematics.net>
2018-11-15 11:17:41 -0500

    Create reverse_ptr(s) from CSV file

--- reverse_ptr.go
+++ reverse_ptr.go
@@ -1,4 +1,4 @@
-//Create reverse_ptr from ipaddress/file
+//Create reverse_ptr(s) from CSV file
 
 package main
 
@@ -9,24 +9,24 @@ import (
 )
 
 func main() {
+		b, err := ioutil.ReadFile("/your/directory") // pass the file name
+	 	if err != nil {
+				fmt.Print(err)
+	 	} //end if error checking
 
-							b, err := ioutil.ReadFile("/your/directory") // just pass the file name
-	 						if err != nil {
-			 						fmt.Print(err)
-	 						}
-							//fmt.Println(b) // print the content as 'bytes'
+		//fmt.Println(b) // print the content as 'bytes'
 
-	 						file := string(b) // convert file to a 'string'
+	 	file := string(b) // convert file to a 'string'
 
-							ipaddress := strings.Split(file, "\r\n") //ipaddress is an array with the delimiter being the next line
+		ipaddress := strings.Split(file, "\r\n") //ipaddress is an array with the delimiter being the next line
 
-							for i := 0; i < len(ipaddress); i++ { //for i != length of the array ipaddress loop
+		for i := 0; i < len(ipaddress); i++ { //for i != length of the array ipaddress loop
 
-									//ipv4addr is an array of the ipaddress[i] split by "." ;
-									ipv4addr := strings.Split(ipaddress[i], ".");
-									//print array for ipv4addr in revervse order and add .in-addr.arpa
-									fmt.Println(ipv4addr[3] + "." + ipv4addr[2] + "." + ipv4addr[1] + "." + ipv4addr[0] + ".in-addr.arpa")
+		//ipv4addr is an array of the ipaddress[i] split by "." ;
+		ipv4addr := strings.Split(ipaddress[i], ".");
+		//print array for ipv4addr in revervse order and add .in-addr.arpa
+		fmt.Println(ipv4addr[3] + "." + ipv4addr[2] + "." + ipv4addr[1] + "." + ipv4addr[0] + ".in-addr.arpa")
 
-							} //end for loop length of array
+		} //end for loop length of array
 
 } //end main
